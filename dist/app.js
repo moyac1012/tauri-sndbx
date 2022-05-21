b5cbb03ad24fa75eb97d8842dc84e62c997275aa
moyac1012 <yuto.tamori@meetasu.jp>
2022-05-21 16:52:12 +0900

    save

---
+++ dist/app.js
@@ -0,0 +1,85 @@
+function print_command(){
+    window.__TAURI__
+        .invoke('print_command')
+}
+
+function rev_string_command(){
+    var display_Msg = document.getElementById('text-msg');
+    var rev_display_Msg = document.getElementById('rev-text-msg');
+    var Msg = document.getElementById('text_area').value;
+    display_Msg.textContent = Msg;
+
+    window.__TAURI__
+        .invoke('rev_string_command', { s: Msg})
+        .then(rev_s => {
+            console.log(rev_s);
+            rev_display_Msg.textContent = rev_s;
+        })
+}
+
+function chat_command(){
+    var chat_name = document.getElementById("name_text_area").value;
+    var chat_msg = document.getElementById("msg_text_area").value;
+    var display_lv = document.getElementById("display_lv");
+    var display_msg = document.getElementById("display_msg");
+    var lv = 0;
+    if(document.getElementById("display_lv").textContent != null){
+        lv = Number(document.getElementById("display_lv").textContent);
+    }
+
+    window.__TAURI__
+        .invoke('chat_command',
+            { text: {
+                name: chat_name,
+                lv: lv,
+                message: chat_msg
+            }})
+        .then(text => {
+            display_lv.textContent = text.lv;
+            display_msg.textContent = text.message;
+        })
+}
+
+function age_command(){
+    var age = parseInt(document.getElementById("age_text_area").value);
+    var display_age = document.getElementById("display_age");
+    console.log(age);
+    if(isNaN(age)){
+        display_age.textContent = "Error: 多分数字じゃないです";
+    }else{
+    window.__TAURI__
+        .invoke('age_command', { age })
+        .then(age_class => {
+            console.log('age_command', age_class)
+            display_age.textContent = age_class;
+        })
+        .catch(e => {
+            console.error('age_command',e)
+            display_age.textContent = e;
+        })
+    }
+}
+
+function file_dialog_command () {
+    window.__TAURI__.dialog
+        .open().then(files => console.log(files));
+}
+
+function ask_command() {
+    let hangman =     "＿＿＿＿＿＿＿<br>　　&nbsp;||　　<br>　Λ||Λ<br>（&nbsp;/&nbsp;⌒ヽ<br>　|&nbsp;|&nbsp;　　|<br>　∪&nbsp;亅|<br>　&nbsp;|　|　|<br>　&nbsp;∪∪<br>　‐ニ三ニ‐";
+    let display_hangman = document.getElementById("display_hangman");
+    window.__TAURI__.dialog
+        .ask("あなたは｜好きですか？", "")
+        .then(ans => {
+            if(ans){
+                display_hangman.innerHTML = hangman;
+            }
+        });
+}
+
+window.__TAURI__.window
+    .appWindow.listen('tauri://move', ({ event, payload }) => {
+            const { x, y } = payload // payload here is a `PhysicalPosition`
+            console.log('x', x);
+            console.log('y', y);
+        })
---
+++ src-tauri/src/main.rs
@@ -0,0 +1,51 @@
+use serde::{ Serialize, Deserialize };
+
+#[derive(Debug, Serialize, Deserialize)]
+struct ChatMessage {
+    name: String,
+    lv: u32,
+    message:String
+}
+
+#[tauri::command]
+fn print_command() {
+  println!("Call from JavaScript");
+}
+
+#[tauri::command]
+fn rev_string_command(s : String) -> String {
+  let rev_s: String = s.chars().rev().collect();
+  rev_s
+}
+
+#[tauri::command]
+fn chat_command(text: ChatMessage) -> ChatMessage {
+  let username = text.name.clone();
+  ChatMessage {
+        name: text.name,
+        lv: text.lv + 1,
+        message: format!("{} : {}", username, text.message)
+    }
+}
+
+#[tauri::command]
+fn age_command(age: i32) -> Result<String, String>{
+  match age {
+      0..=19 => Ok(format!("未成年")),
+      20..=125 => Ok(format!("成人")),
+      126.. => Err(format!("死んでいます")),
+      _ => Err(format!("生まれてません")),
+  }
+}
+
+fn main() {
+  tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![
+      print_command,
+      rev_string_command,
+      chat_command,
+      age_command,
+    ])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
+}
