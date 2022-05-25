9c60645e6fc41d391c311c4da76a7f7a04476d97
moyac1012 <yuto.tamori@meetasu.jp>
2022-05-25 16:10:24 +0900

    ドラックアンドドロップでzipを複数作る事に対応

--- dist/filedrop_listener.js
+++ dist/filedrop_listener.js
@@ -1,8 +1,20 @@
+function zip_command(filepaths){
+    let zip_output_path_value =  document.getElementById("zip_output_path_text_area").value;
+    if(zip_output_path_value == ""){
+        zip_output_path_value = "./"
+    }
+    window.__TAURI__
+        .invoke("zip_command", {filepaths: filepaths, outputpath: zip_output_path_value})
+        .then(result => {
+            console.log(result);
+            display_zip_result.textContent = result
+    });
+}
 
 window.__TAURI__.event
     .listen("tauri://file-drop-hover", ({event, payload}) => {
         console.log("file-drop-hover", payload);
-        
+
     })
 
 window.__TAURI__.event
@@ -14,9 +26,11 @@ window.__TAURI__.event
     .listen("tauri://file-drop", ({event, payload}) => {
         let display_files = document.getElementById("display_files");
         let text = "";
-        payload.forEach(element => {
-            text += element;
+        payload.forEach(filepath => {
+            text += filepath;
             text += "\n";
         });
         display_files.textContent = text;
+
+        zip_command(payload);
     })
